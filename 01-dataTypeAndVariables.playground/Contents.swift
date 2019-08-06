87bbf38d4866ad0edfa51d8fa7a329fcc0ac21f8
Alencia <alencia@Apples-MacBook-Pro-18.local>
2019-08-06 07:38:09 -0700

    KWK-Day1

---
+++ 01-dataTypeAndVariables.playground/Contents.swift
@@ -0,0 +1,81 @@
+import UIKit
+
+// Example of strings
+var str = "Hello, playground"
+"puppies"
+"1234"
+"Hello, world"
+
+//Example of a variable
+var address = "10 Hudson Yard"
+address = "48th floor"
+var companyName : String
+companyName = "Best Company Ever"
+
+// Example of a constant
+let planet = " Earth"
+
+var string1 = "Welcome"
+var string2 = "Alencia"
+
+var welcome1 = "\(string1) \(string2)"
+var welcome2 = string1 + " " + string2
+
+print(welcome1)
+
+//Practice
+var first = "Karlie"
+var last = "Kloss"
+
+print("\(first)\(last)")
+print("\(last)\(first)")
+var name = "\(first) \(last)"
+print(name)
+var name2 = "\(last) \(first) \(last) \(first)"
+print(name2)
+print ("I Love \(first)")
+
+//Examples of integers
+1
+54
+-16
+
+//Examples of floats and doubles
+0.13
+3.14
+2.1
+
+var integer = 4.0
+var double = 5.0
+
+4 * 4
+4 * integer
+4 * double
+integer * double
+
+// Practice of strings
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+(a + b + c + d) / 4
+a + b * c/d
+(a+(b*c))/d
+(a+b)*(c/d)
+
+// Part 2
+var pay = 10.25 * 20
+print (pay)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MyPlayground.Conditionals .playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+var str = "Hello, playground"
+5 < 3
+12 > 7
+6 != 8
+"karlie" == "karlie"
+"karlie" == "karliekloss"
+"Karlie" == "karlie"
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+
+// Examples of Conditionals
+var dogAge = 13
+if dogAge < 2 {
+    print("You are a puppy!🐶")
+}
+else if dogAge > 12 {
+    print("You are elderly.")
+}
+else {
+    print("You are awesome!")
+}
+// Practice - Conditionals
+var favoriteFood = "Pasta"
+if favoriteFood == "Chipotle" {
+    print ("Chipotle is better than Pasta")
+}
+
+//Part 1
+var hasAccount = "false"
+if hasAccount == "false" {
+    print ("Please log in!")
+}
+ var gradeLevel = "12"
+if gradeLevel <= "12" {
+    print ("High School")
+}
+
+
